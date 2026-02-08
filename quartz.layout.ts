efac358449211a0a0c0321b123712c7119e06832
张恒瑞 <zhanghengrui@ZHenryMacBook-Air.local>
2026-02-08 08:48:34 +0800

    Quartz sync: Feb 8, 2026, 8:48 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
+      GitHub: "https://github.com/zhenrys/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
